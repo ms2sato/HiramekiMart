be6ad7418be50b3b016f0bcae6185097f91d0cf3
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2016-12-21 00:14:51 +0900

    ログインチェック機能追加

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,9 +1,21 @@
 class SupportsController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :set_item
+  before_action :store_event_url
+  before_action :authenticate_user!
+
   def buy
-    item = Item.find(params[:item_id])
-    support = current_user.supports.build(item_id: item.id)
+    support = current_user.supports.build(item_id: @item.id)
     support.save!
-    redirect_to item_path(item.id)
+    redirect_to item_path(@item.id)
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def store_event_url
+    session[:user_return_to] = item_path(@item) unless user_signed_in?
   end
 end
